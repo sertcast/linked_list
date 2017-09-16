5df6a814362f284fb00064699043dfaaa24b00f8
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2017-09-16 12:07:55 -0400

    created files

---
+++ linked_list/linked_list.c
@@ -0,0 +1,9 @@
+//
+//  linked_list.c
+//  linked_list
+//
+//  Created by Mert Kaya on 2017-09-16.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#include "linked_list.h"
---
+++ linked_list/linked_list.h
@@ -0,0 +1,14 @@
+//
+//  linked_list.h
+//  linked_list
+//
+//  Created by Mert Kaya on 2017-09-16.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#ifndef linked_list_h
+#define linked_list_h
+
+#include <stdio.h>
+
+#endif /* linked_list_h */
