fa60df7ac5be168aa72bd214a79cd3f17f973c53
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2017-09-16 12:06:23 -0400

    Initial Commit

---
+++ linked_list/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  linked_list
+//
+//  Created by Mert Kaya on 2017-09-16.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
